b216727afa355b08c88c848c0357a10c9c43dccb
Aman Desai <amanpdesai@gmail.com>
2021-07-08 17:02:52 -0700

    Project work
    Worked on first person control and camera and other basic assets

---
+++ MRTK Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            transform.SetPositionAndRotation(new Vector3(0, 0.94f, 0), transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
